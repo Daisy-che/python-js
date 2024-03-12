ddda73a55610b76fe3bc21e550b498f939edc9ed
Student <daisychepkiruih@gmail.com>
2024-03-12 22:01:42 +0300

    add file

--- string.js
+++ string.js
@@ -1,14 +1,16 @@
+//Write a JavaScript expression using the modulus operator to find the remainder when 15 is divided by 4.
 let a=15%4;
 console.log(a)
-
+//Write a JavaScript expression to concatenate two strings.
 let b ="Daisy";
 let greeting="hello";
 console.log(b +" "+greeting)
 
+//Declare a variable in JavaScript and assign it the value of a number. Determine its data type using the typeof operator.
 //data types
 let age= 25;
 console.log('age' ,typeof age);
-
+//Create an array in JavaScript and demonstrate how to access the third element.
 //array
 let array =[1,2,3,4];
 let thirdElement=array[2];
