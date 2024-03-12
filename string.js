c0550e3b35a859f693f0bc0e479a6306613471bb
Student <daisychepkiruih@gmail.com>
2024-03-12 20:49:16 +0300

    add file

---
+++ string.js
@@ -0,0 +1,18 @@
+let a=15%4;
+console.log(a)
+
+let b ="Daisy";
+let greeting="hello";
+console.log(b +" "+greeting)
+
+//data types
+let age= 25;
+console.log('age' ,typeof age);
+
+//array
+let array =[1,2,3,4];
+let thirdElement=array[2];
+console.log(thirdElement);
+
+
+
---
+++ string.py
@@ -0,0 +1,23 @@
+//python
+a=8
+b=3
+c=8/3
+print(c)
+multiplication
+d=4
+e=5
+f=d*e
+print(f)
+Data Types
+pi= 3.14
+print(pi)
+
+person-info{
+"name"="Daisy"
+"age"=21
+"city"= "Nairobi"
+}
+
+print(person-info)
+
+
